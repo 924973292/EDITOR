0d0d42aa64444832c7cd47456d9b7866cb7d6622
YHY <924973292@qq.com>
2023-06-27 20:32:56 +0800

    YHY

--- train_t2t.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source activate unilite
-export DETETRON2_DATASETS=/15127306268
-cd /15127306268/wyh/UIS/
-python train_net.py --config_file /15127306268/wyh/UIS/configs/MSMT17/t2tvit.yml
