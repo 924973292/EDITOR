fc4cf0f510751a0b132e99d2e269cacd419c0a17
YHY <924973292@qq.com>
2023-07-21 18:27:20 +0800

    YHY

--- modeling/fusion_part/MMDA.py
+++ modeling/fusion_part/MMDA.py
@@ -140,6 +140,77 @@ class MLP(nn.Module):
         return out
 
 
+class GeM(nn.Module):
+    def __init__(self, p=3, eps=1e-6):
+        super(GeM, self).__init__()
+        self.p = nn.Parameter(torch.ones(1) * p)
+        self.eps = eps
+
+    def forward(self, x):
+        return self.gem(x, p=self.p, eps=self.eps)
+
+    def gem(self, x, p=3, eps=1e-6):
+        return F.avg_pool2d(x.clamp(min=eps).pow(p), (x.size(-2), x.size(-1))).pow(1. / p)
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(' + 'p=' + '{:.4f}'.format(self.p.data.tolist()[0]) + ', ' + 'eps=' + str(
+            self.epls) + ')'
+
+
+class LocalRefinementUnits(nn.Module):
+    def __init__(self, dim, out_dim=768, kernel=1):
+        super().__init__()
+        self.channels = dim
+        self.out_dim = out_dim
+        self.dwconv = nn.Conv2d(self.channels, self.channels, kernel, 1, padding=0, groups=self.channels)
+        self.bn1 = nn.BatchNorm2d(self.channels)
+        self.ptconv = nn.Conv2d(self.channels, self.out_dim, 1, 1)
+        self.bn2 = nn.BatchNorm2d(self.out_dim)
+        self.act1 = nn.PReLU()
+        self.act2 = nn.PReLU()
+
+    def forward(self, x):
+        x = self.act1(self.bn1(self.dwconv(x)))
+        x = self.act2(self.bn2(self.ptconv(x)))
+        return x
+
+
+class ChannelUnite(nn.Module):
+    def __init__(self, dim, outdim, kernel):
+        super().__init__()
+        self.local = LocalRefinementUnits(dim=dim, out_dim=outdim, kernel=kernel)
+        self.avg_pool = GeM()
+
+    def forward(self, x, weight, height):
+        b, c, _, _ = x.size()
+        x = self.local(x)
+        y = self.avg_pool(x).view(b, c)
+        return y
+
+
+class ChannelEnchance(nn.Module):
+    def __init__(self, dim, outdim):
+        super().__init__()
+        self.local_1 = ChannelUnite(dim=dim, outdim=outdim, kernel=1)
+        self.local_3 = ChannelUnite(dim=dim, outdim=outdim, kernel=3)
+        self.local_5 = ChannelUnite(dim=dim, outdim=outdim, kernel=5)
+        self.local_7 = ChannelUnite(dim=dim, outdim=outdim, kernel=7)
+        self.act = nn.Sigmoid()
+
+        print('New FFN here!!!')
+
+    def forward(self, x, weight, height):
+        b, l, c = x.shape
+        x = x.reshape(b, weight, height, c).permute(0, 3, 1, 2)
+        c_1 = self.local_1(x, weight, height)
+        c_3 = self.local_3(x, weight, height)
+        c_5 = self.local_5(x, weight, height)
+        c_7 = self.local_7(x, weight, height)
+        c = self.act(c_1 + c_3 + c_5 + c_7).reshape(b, c, 1, 1)
+        out = (x * c).flatten(2).transpose(1, 2)
+        return out
+
+
 class FuseUnit(nn.Module):
     def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
                  drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm, mode=0):
@@ -160,7 +231,8 @@ class FuseUnit(nn.Module):
         # NOTE: drop path for stochastic depth, we shall see if this is better than dropout here
         self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
         self.norm2 = norm_layer(dim)
-        self.mlp = MLP(channel=dim)
+        # self.mlp = MLP(channel=dim)
+        self.mlp = ChannelEnchance(dim=dim, outdim=dim)
 
     def forward(self, ma, mb, mc, weight=16, height=8):
         if self.mode == 0:
@@ -308,7 +380,7 @@ class RotationAttention(nn.Module):
             y_cls = self.Rotation1(y[:, 0, :], y[:, 1:, :])
             z_cls = self.Rotation1(z[:, 0, :], z[:, 1:, :])
             cls = torch.cat([x_cls, y_cls, z_cls], dim=-1)
-            patch = torch.mean(self.local_enchance(x[:, 1:, :], y[:, 1:, :], z[:, 1:, :]),dim=-2)
+            patch = torch.mean(self.local_enchance(x[:, 1:, :], y[:, 1:, :], z[:, 1:, :]), dim=-2)
 
             return cls, patch
 
