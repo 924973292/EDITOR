29299eff042f95e2c71e7e689b114f66de5a7405
Yuhao Wang <89966785+924973292@users.noreply.github.com>
2024-04-03 15:41:34 +0800

    Update Visualization for Reproduction

--- modeling/fusion_part/Frequency.py
+++ modeling/fusion_part/Frequency.py
@@ -7,61 +7,6 @@ import torch.nn.functional as F
 from pytorch_wavelets import DWTForward, DWTInverse
 
 
-def display_image(image_path, mode=1):
-    pre_fix = '/13994058190/WYH/EDITOR/data/RGBNT201/train_171/'
-    if mode == 1:
-        pre_fix = pre_fix + 'RGB/'
-    elif mode == 2:
-        pre_fix = pre_fix + 'NI/'
-    elif mode == 3:
-        pre_fix = pre_fix + 'TI/'
-    image = Image.open(pre_fix + image_path)
-    resized_image = image.resize((128, 256))  # Resize to 256x128
-    plt.imshow(resized_image)
-    plt.axis('off')
-    plt.show()
-
-
-# Visualize the mask on the image
-def visualize_multiple_masks(images, masks, mode, pre_fix, writer=None, epoch=None):
-    num_images_to_display = 12  # Number of images to display
-    images = images[:num_images_to_display]
-    num_rows = 2  # Number of rows in the display grid
-    num_cols = 6  # Number of columns in the display grid
-    fig, axes = plt.subplots(num_rows, num_cols, figsize=(15, 6))
-
-    for i in range(num_images_to_display):
-        if mode == 1:
-            prefix = pre_fix + 'RGB/'
-        elif mode == 2:
-            prefix = pre_fix + 'NI/'
-        elif mode == 3:
-            prefix = pre_fix + 'TI/'
-        # Load the original image
-        image = Image.open(prefix + images[i])
-        original_image = image.resize((128, 256))  # Resize to 256x128
-
-        # Convert the image to numpy array
-        original_np = np.array(original_image)
-
-        row = i // num_cols
-        col = i % num_cols
-
-        # Display the masked image
-        axes[row, col].imshow(original_np)
-        axes[row, col].axis('off')
-    plt.tight_layout()
-    plt.show()
-    if writer is not None:
-        if mode == 1:
-            sign = 'RGB'
-        elif mode == 2:
-            sign = 'NIR'
-        elif mode == 3:
-            sign = 'TIR'
-        writer.add_figure('FREQUENCY_' + sign, fig, global_step=epoch)
-
-
 class Frequency_based_Token_Selection(nn.Module):
     def __init__(self, keep,stride=16):
         super().__init__()
@@ -70,6 +15,8 @@ class Frequency_based_Token_Selection(nn.Module):
         self.keep = keep
         self.window_size = 16
         self.stride = stride
+
+    # Here, the show function can produce the Fig.4 in the paper.
     def show(self, x, writer=None, epoch=None, img_path=None, mode=1):
         x = x[:12]
         num_rows = 2  # Number of rows in the display grid
@@ -91,6 +38,7 @@ class Frequency_based_Token_Selection(nn.Module):
             elif mode == 2:
                 writer.add_figure('FREQUENCY_Before', fig, global_step=epoch)
 
+    
     def mask(self, Inverse,window_size=16):
         batch_size, height, width = Inverse.size(0), Inverse.size(-2), Inverse.size(-1)
         Inverse = torch.mean(Inverse, dim=1)
@@ -117,6 +65,7 @@ class Frequency_based_Token_Selection(nn.Module):
     def forward(self, x, y, z, img_path, pattern='a', mode=None, writer=None, step=None):
         Ylx, Yhx = self.DWT(x)
         Yly, Yhy = self.DWT(y)
+        # You can try to insert the self.show here to reproduce the Fig.4
         if z!=None:
             Ylz, Yhz = self.DWT(z)
             low = (Ylx+Yly+Ylz)/3
