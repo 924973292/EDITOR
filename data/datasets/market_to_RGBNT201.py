577835aaeabe2c0f82690b0664f90c0a64414b86
Yuhao Wang <89966785+924973292@users.noreply.github.com>
2024-04-04 23:24:25 +0800

    Add Market1501-MM for evaluation

---
+++ data/datasets/market_to_RGBNT201.py
@@ -0,0 +1,96 @@
+from __future__ import division, print_function, absolute_import
+import glob
+import os.path as osp
+import warnings
+
+import os.path as osp
+from .bases import BaseImageDataset
+
+
+class market_to_RGBNT201(BaseImageDataset):
+    dataset_dir = 'Market1501-MM'
+
+    def __init__(self, root='',verbose=True, **kwargs):
+        self.root = osp.abspath(osp.expanduser(root))
+        self.dataset_dir = osp.join(self.root, self.dataset_dir)
+
+        # allow alternative directory structure
+        self.data_dir = self.dataset_dir
+        data_dir = osp.join(self.data_dir)
+        if osp.isdir(data_dir):
+            self.data_dir = data_dir
+        else:
+            warnings.warn(
+                'The current data structure is deprecated.'
+            )
+        
+        
+        self.train_dir = osp.join(self.data_dir, 'train')
+        self.query_dir = osp.join(self.data_dir, 'query')
+        self.gallery_dir = osp.join(self.data_dir, 'gallery')
+
+
+        self._check_before_run()
+
+        train = self.process_dir(self.train_dir, relabel=True)
+        query = self.process_dir(self.query_dir, relabel=False)
+        gallery = self.process_dir(self.gallery_dir, relabel=False)
+
+        if verbose:
+            print("=> Market1501-MM loaded")
+            self.print_dataset_statistics(train, query, gallery)
+
+        self.train = train
+        self.query = query
+        self.gallery = gallery
+
+        self.num_train_pids, self.num_train_imgs, self.num_train_cams, self.num_train_vids = self.get_imagedata_info(
+            self.train)
+        self.num_query_pids, self.num_query_imgs, self.num_query_cams, self.num_query_vids = self.get_imagedata_info(
+            self.query)
+        self.num_gallery_pids, self.num_gallery_imgs, self.num_gallery_cams, self.num_gallery_vids = self.get_imagedata_info(
+            self.gallery)
+    def _check_before_run(self):
+        """Check if all files are available before going deeper"""
+        if not osp.exists(self.dataset_dir):
+            raise RuntimeError("'{}' is not available".format(self.dataset_dir))
+        if not osp.exists(self.train_dir):
+            raise RuntimeError("'{}' is not available".format(self.train_dir))
+        if not osp.exists(self.query_dir):
+            raise RuntimeError("'{}' is not available".format(self.query_dir))
+        if not osp.exists(self.gallery_dir):
+            raise RuntimeError("'{}' is not available".format(self.gallery_dir))
+
+    def process_dir(self, dir_path, relabel=False):
+        img_paths_RGB = glob.glob(osp.join(dir_path, 'RGB', '*.jpg'))
+        pid_container = set()
+        for img_path_RGB in img_paths_RGB:
+            jpg_name = img_path_RGB.split('/')[-1]
+            pid = int(jpg_name.split('_')[0])
+            if pid == -1:
+                continue # junk images are just ignored
+            pid_container.add(pid)
+        pid2label = {pid: label for label, pid in enumerate(pid_container)}
+
+        data = []
+        for img_path_RGB in img_paths_RGB:
+            img = []
+            jpg_name = img_path_RGB.split('/')[-1]
+            img_path_NI = osp.join(dir_path, 'NI', jpg_name)
+            img_path_TI = osp.join(dir_path, 'TI', jpg_name)
+            img.append(img_path_RGB)
+            img.append(img_path_NI)
+            img.append(img_path_TI)
+            pid = int(jpg_name.split('_')[0])
+            camid = int(jpg_name.split('_')[1][1])
+            if pid == -1:
+                continue # junk images are just ignored
+            assert 0 <= pid <= 1501 # pid == 0 means background
+            assert 1 <= camid <= 6
+            camid -= 1 # index starts from 0
+            trackid = -1
+            if relabel:
+                pid = pid2label[pid]
+            data.append((img, pid, camid, trackid))
+        # print(data[0], '\n', data[1000], '\n', data[2000])
+        return data
